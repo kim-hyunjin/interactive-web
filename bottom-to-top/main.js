cbc9f336fcc67d3628f7c1e0e9a9fc4f54ae4b9d
kim-hyunjin <hyunjin1612@gmail.com>
2022-02-21 15:12:24 +0900

    스크롤에 따라 이미지 보여주기

---
+++ bottom-to-top/main.js
@@ -0,0 +1,13 @@
+const html = document.documentElement;
+const body = document.querySelector("body");
+const imgBox = document.getElementById("imgBox");
+const img = document.getElementById("long-img");
+
+body.style.height = `${img.clientHeight}px`;
+let initialTop = -img.clientHeight + window.innerHeight;
+
+imgBox.style.top = `${initialTop}px`;
+window.addEventListener("scroll", () => {
+  const scrollTop = html.scrollTop;
+  imgBox.style.top = `${initialTop + scrollTop}px`;
+});
