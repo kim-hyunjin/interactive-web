381629cd4c48f23f7443dead7b7d87e136e51710
kim-hyunjin <hyunjin1612@gmail.com>
2023-08-15 18:51:28 +0900

    feat: draw particles

--- firework/index.js
+++ firework/index.js
@@ -1,23 +1,36 @@
 import {Canvas} from "./js/canvas.js";
 import {Particle} from "./js/particle.js";
+import {randomNumBetween} from "./js/utils.js";
 
 let canvas;
 
 const particles = [];
-const PARTICLE_COUNT = 10;
+const PARTICLE_COUNT = 400;
 
 function createParticles() {
+    const x = randomNumBetween(0, canvas.width);
+    const y = randomNumBetween(0, canvas.height);
     for (let i = 0; i < PARTICLE_COUNT; i++) {
-        const x = Math.random() * canvas.width;
-        const y = Math.random() * canvas.height;
-        particles.push(new Particle(canvas, x, y));
+        const r = randomNumBetween(2, 100) * 0.2;
+        const angle = Math.PI / 180 * randomNumBetween(0, 360);
+        const vx = r * Math.cos(angle);
+        const vy = r * Math.sin(angle);
+        const opacity = randomNumBetween(0.6, 0.9)
+
+        particles.push(new Particle(canvas, x, y, vx, vy, opacity));
     }
 }
 
-function drawParticles() {
-    particles.forEach(particle => {
+function draw() {
+    canvas.ctx.fillStyle = '#00000040'
+    canvas.ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    particles.forEach((particle, index) => {
         particle.update();
         particle.draw();
+        if (particle.opacity <= 0) {
+            particles.splice(index, 1);
+        }
     });
 }
 
@@ -26,7 +39,7 @@ window.addEventListener('load', () => {
     canvas = new Canvas(canvasEl);
     canvas.init();
     createParticles();
-    canvas.render(drawParticles);
+    canvas.render(draw);
 })
 
 window.addEventListener('resize', () => {
--- firework/js/particle.js
+++ firework/js/particle.js
@@ -1,19 +1,31 @@
 export class Particle {
-    constructor(canvas, x, y) {
+    constructor(canvas, x, y, vx, vy, opacity) {
         this.canvas = canvas;
         this.ctx = this.canvas.ctx;
         this.x = x;
         this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.opacity = opacity;
+        this.gravity = 0.12;
+        this.friction = 0.93;
     }
 
     update() {
+        this.vy += this.gravity;
 
+        this.vx *= this.friction;
+        this.vy *= this.friction;
+
+        this.x += this.vx;
+        this.y += this.vy;
+        this.opacity -= 0.01;
     }
 
     draw() {
-        console.log('draw', this.ctx, this.x, this.y)
+        this.ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`
         this.ctx.beginPath()
-        this.ctx.arc(this.x, this.y, 50, 0, Math.PI * 2)
+        this.ctx.arc(this.x, this.y, 2, 0, Math.PI * 2)
         this.ctx.fill()
         this.ctx.closePath()
     }
