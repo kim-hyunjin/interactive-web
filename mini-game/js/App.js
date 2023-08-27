1a6779f94900e3cefc6c82e22b8b2f9bc6a7efd5
kim-hyunjin <hyunjin1612@gmail.com>
2023-08-27 17:25:20 +0900

    미니게임만들기 - 배경 그리기

--- mini-game/js/App.js
+++ mini-game/js/App.js
@@ -1,3 +1,5 @@
+import Background from "./Background.js";
+
 export default class App {
     static canvas = document.querySelector('canvas');
     static ctx = App.canvas.getContext('2d');
@@ -9,6 +11,11 @@ export default class App {
 
     constructor() {
         window.addEventListener('resize', this.resize.bind(this));
+        this.backgrounds = [
+            new Background({ctx: App.ctx, height: App.height, img: document.querySelector('#bg3-img'), speed: -1}),
+            new Background({ctx: App.ctx, height: App.height, img: document.querySelector('#bg2-img'), speed: -2}),
+            new Background({ctx: App.ctx, height: App.height, img: document.querySelector('#bg1-img'), speed: -4}),
+        ]
     }
 
     resize() {
@@ -34,6 +41,13 @@ export default class App {
             }
             then = now - (delta % App.interval);
             App.ctx.clearRect(0, 0, App.width, App.height);
+
+            // 배경 관리
+            this.backgrounds.forEach(background => {
+                background.update()
+                background.draw()
+            });
+
             App.ctx.fillRect(50, 50, 100, 100)
         }
         frame();
