95e71ef61235c5214f6900dabca1946138bf1b19
kim-hyunjin <hyunjin1612@gmail.com>
2023-09-02 22:04:09 +0900

    draw image

--- nudake/src/containers/Nudake.jsx
+++ nudake/src/containers/Nudake.jsx
@@ -2,6 +2,10 @@ import { useEffect } from "react";
 import "../styles/containers/Nudake.css";
 import { useRef } from "react";
 
+import image1 from "../assets/nudake-1.jpg";
+import image2 from "../assets/nudake-2.jpg";
+import image3 from "../assets/nudake-3.jpg";
+
 const Nudake = () => {
   const canvasRef = useRef(null);
 
@@ -9,6 +13,9 @@ const Nudake = () => {
     const canvas = canvasRef.current;
     const canvasParent = canvas.parentNode;
     const ctx = canvas.getContext("2d");
+
+    const imageSrcs = [image1, image2, image3];
+    let currentIndex = 0;
 
     let canvasWidh, canvasHeight;
 
@@ -21,9 +28,20 @@ const Nudake = () => {
 
       canvas.width = canvasWidh;
       canvas.height = canvasHeight;
-    }
-    window.addEventListener("resize", resize);
 
+      drawImage();
+    }
+
+    function drawImage() {
+      ctx.clearRect(0, 0, canvasWidh, canvasHeight);
+      const image = new Image();
+      image.src = imageSrcs[currentIndex];
+      image.onload = () => {
+        ctx.drawImage(image, 0, 0, canvasWidh, canvasHeight);
+      };
+    }
+
+    window.addEventListener("resize", resize);
     resize();
 
     return () => {
